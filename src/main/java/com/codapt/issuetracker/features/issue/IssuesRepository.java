b901dd41325a137be49fcf246772d6f8154bd30b
= <=>
2024-04-23 11:01:48 +0200

    adds repositiries for users and issues

---
+++ src/main/java/com/codapt/issuetracker/features/issue/IssuesRepository.java
@@ -0,0 +1,19 @@
+package com.codapt.issuetracker.features.issue;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import com.codapt.issuetracker.features.users.User;
+import com.codapt.issuetracker.shared.enums.IssueStatus;
+
+
+
+@Repository
+public interface IssuesRepository extends CrudRepository<Issue, Long> {
+
+    /** Get issues created by a certain user */
+    List<Issue> findByCreatedBy(User createdBy);
+
+    /** Get issues at a certain status */
+    List<Issue> findByStatus(IssueStatus status);
+}
